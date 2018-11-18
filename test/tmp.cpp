d8f7907adbb718467ff2ab0e651920bc26a8ceba
zhenyus <sunnyszy@gmail.com>
2018-11-18 11:03:56 -0500

    fix: remove tmp file from source control

--- test/tmp.cpp
+++ test/tmp.cpp
@@ -13,8 +13,12 @@ int main()
 
     using namespace boost::bimaps;
     bimap<set_of<std::pair<uint64_t, uint64_t>>, list_of<uint64_t>> myMap2;
+    myMap2.left[std::make_pair<uint64_t, uint64_t>(1,1)] = 3;
     myMap2.left[std::make_pair<uint64_t, uint64_t>(1,1)] = 1;
     myMap2.left[std::make_pair<uint64_t, uint64_t>(1,1)] = 2;
+    myMap2.left[std::make_pair<uint64_t, uint64_t>(1,1)] = 4;
+    myMap2.left[std::make_pair<uint64_t, uint64_t>(1,1)] = 5;
+    myMap2.left[std::make_pair<uint64_t, uint64_t>(1,1)] = 0;
     // myMap2.left["key2"] = 3;
 
     // for (auto&& elem : myMap2.left)
@@ -26,9 +30,4 @@ int main()
 
     auto it = myMap2.right.begin();
     std::cout<<it->first<<" "<<(it->second).first<<endl;
-    std::cout << "Using Boost "
-              << BOOST_VERSION / 100000     << "."  // major version
-              << BOOST_VERSION / 100 % 1000 << "."  // minor version
-              << BOOST_VERSION % 100                // patch level
-              << std::endl;
 }
