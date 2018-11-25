1c80fad266aea85664efa7df9d6342dd101a4c8b
zhenyus <sunnyszy@gmail.com>
2018-11-25 09:54:18 -0500

    refactor: rewrite LR with more efficient sampling data structure

--- include/caches/random_variants.h
+++ include/caches/random_variants.h
@@ -14,6 +14,8 @@
 #include <boost/bimap.hpp>
 #include <boost/bimap/multiset_of.hpp>
 
+const uint8_t max_n_past_intervals = 4;
+
 
 using namespace std;
 
@@ -42,71 +44,121 @@ public:
 static Factory<RandomCache> factoryRandom("Random");
 
 
-//class LRCache : public Cache
-//{
-//public:
-//    // from id to intervals
+class Meta {
+public:
+    static uint8_t _n_past_intervals;
+    uint64_t _key;
+    uint64_t _size;
+    uint64_t _future_timestamp;
+    uint8_t _past_timestamp_idx;
+    uint64_t _past_timestamps[max_n_past_intervals];
+
+    Meta(const uint64_t & key, const uint64_t & size, const uint64_t & past_timestamp, const uint64_t & future_timestamp) {
+        _key = key;
+        _size = size;
+//        _past_timestamps = new uint64_t[_n_past_intervals];
+        _past_timestamps[0] = past_timestamp;
+        _future_timestamp = future_timestamp;
+        _past_timestamp_idx = _past_timestamp_idx + (uint8_t) 1;
+    }
+
+//    ~Meta() {
+//        delete []_past_timestamps;
+//    }
+// todo: custom assign function
+
+    inline void append_past_timestamp(const uint64_t & past_timestamp) {
+        _past_timestamps[_past_timestamp_idx%_n_past_intervals] = past_timestamp;
+        _past_timestamp_idx = _past_timestamp_idx + (uint8_t) 1;
+        //todo: can use bit-wise
+        // prevent overflow
+        if (_past_timestamp_idx >= _n_past_intervals * 2)
+            _past_timestamp_idx -= _n_past_intervals;
+    }
+};
+
+
+class LRCache : public Cache
+{
+public:
+    // from id to intervals
 //    std::unordered_map<std::pair<uint64_t, uint64_t >, std::list<uint64_t> > past_timestamps;
 //    boost::bimap<boost::bimaps::set_of<KeyT>, boost::bimaps::multiset_of<uint64_t>> future_timestamp;
+    map<uint64_t, set<uint64_t>> gc_timestamp;
+    //key -> (0/1 list, idx)
+    unordered_map<uint64_t, pair<bool, uint32_t>> key_map;
+    vector<Meta> meta_holder[2];
+
 //    std::unordered_map<KeyT, uint64_t> unordered_future_timestamp;
-//    // sample_size
-//    uint64_t sample_rate=32;
-//    // threshold
-//    uint64_t threshold=1000000;
-//    double log1p_threshold=log1p(threshold);
-//    // batch_size
-//    uint64_t batch_size=10;
-//    // learning_rate
-//    double learning_rate=0.000001;
-//    // n_past_interval
-//    uint64_t n_past_intervals = 4;
-//
-//    double * weights;
-//    double bias;
-//    double mean_diff=0;
-//    std::unordered_map<uint64_t, double *> pending_gradients;
-//
-//
-//    LRCache()
-//        : RandomCache()
-//    {
-//    }
-//    virtual ~LRCache()
-//    {
-//        delete []weights;
-//    }
-//
-//    virtual void setPar(std::string parName, std::string parValue) {
-//        if (parName == "sample_rate")
-//            sample_rate = stoull(parValue);
-//        else if(parName == "threshold") {
-//            threshold = stoull(parValue);
-//            log1p_threshold = std::log1p(threshold);
-//        }
-//        else if(parName == "batch_size")
-//            batch_size = stoull(parValue);
-//        else if(parName == "learning_rate")
-//            learning_rate = stod(parValue);
-//        else if(parName == "n_past_intervals") {
-//            n_past_intervals = stoull(parValue);
-//            weights = new double[n_past_intervals];
-//            for (int i = 0; i < n_past_intervals; i++) {
-//                weights[i] = 0;
-//            }
-//            bias = 0;
-//        }
-//        else {
-//       std::cerr << "unrecognized parameter: " << parName << std::endl;
-//   }
-//}
-//    virtual bool lookup(SimpleRequest& req);
-//    virtual void admit(SimpleRequest& req);
-//    virtual void evict(uint64_t t);
+    // sample_size
+    uint8_t sample_rate;
+    // threshold
+    uint64_t threshold;
+    double log1p_threshold;
+    // batch_size
+    uint64_t batch_size;
+    // learning_rate
+    double learning_rate;
+    // n_past_interval
+    uint8_t n_past_intervals;
+
+    double * weights;
+    double bias = 0;
+    double mean_diff=0;
+    std::unordered_map<uint64_t, double *> pending_gradients;
+
+    //todo: seed and generator
+    default_random_engine _generator = default_random_engine();
+    uniform_int_distribution<std::size_t> _distribution = uniform_int_distribution<std::size_t>();
+
+    LRCache()
+        : Cache()
+    {
+    }
+    virtual ~LRCache()
+    {
+        delete []weights;
+    }
+
+    virtual void setPar(std::string parName, std::string parValue) {
+        if (parName == "sample_rate")
+            sample_rate = stoull(parValue);
+        else if(parName == "threshold") {
+            threshold = stoull(parValue);
+            log1p_threshold = std::log1p(threshold);
+        }
+        else if(parName == "batch_size")
+            batch_size = stoull(parValue);
+        else if(parName == "learning_rate")
+            learning_rate = stod(parValue);
+        else if(parName == "n_past_intervals") {
+            n_past_intervals = (uint8_t) stoi(parValue);
+            if (n_past_intervals > max_n_past_intervals) {
+                cerr << "n_past_intervals exceeds max limitation: "<<max_n_past_intervals<<endl;
+                return;
+            }
+            weights = new double[n_past_intervals];
+            for (int i = 0; i < n_past_intervals; i++) {
+                weights[i] = 0;
+            }
+            Meta::_n_past_intervals = n_past_intervals;
+        }
+        else {
+       std::cerr << "unrecognized parameter: " << parName << std::endl;
+   }
+}
+    virtual bool lookup(SimpleRequest& req);
+    virtual void admit(SimpleRequest& req);
+    virtual void evict(const uint64_t & t);
+    void evict(SimpleRequest & req) {};
+    void evict() {};
+    //sample, rank the 1st and return
+    pair<uint64_t, uint32_t > rank(const uint64_t & t);
 //    void try_train(double * gradients);
 //    void try_gc(uint64_t t);
-//};
-//
-//static Factory<LRCache> factoryLR("LR");
+};
+
+static Factory<LRCache> factoryLR("LR");
 
 
 class BeladySampleCache : public RandomCache
--- src/caches/random_variants.cpp
+++ src/caches/random_variants.cpp
@@ -36,53 +36,61 @@ void RandomCache::evict() {
     object_size.erase(key);
 }
 
-//bool LRCache::lookup(SimpleRequest &_req) {
-//    AnnotatedRequest & req = static_cast<AnnotatedRequest &>(_req);
-//    auto key = make_pair(req._id, req._size);
-//    //add timestamp
-//    auto &_past_timestamps = past_timestamps[key];
-//    const auto n_past_timestamps = _past_timestamps.size();
-//
-//    //update past timestamps
-//    if (n_past_timestamps >= n_past_intervals)
-//        _past_timestamps.pop_back();
-//    _past_timestamps.push_front(req._t);
-//
-//    uint64_t _future_timestamp;
-//    if (req._t + threshold > req._next_t)
-//        _future_timestamp = req._next_t;
-//    else
-//        _future_timestamp = req._t + threshold;
-//    //update future timestamp. Can look only threshold far
-//    auto it = unordered_future_timestamp.find(key);
-//    if (it == unordered_future_timestamp.end()) {
-//        //insert
-//        unordered_future_timestamp.insert({{req._id, req._size}, _future_timestamp});
-//        future_timestamp.insert({{req._id, req._size}, _future_timestamp});
-//    } else {
-//        //if has pending training data, put it
-//        auto gradients = pending_gradients.find(req._t);
-//        if (gradients != pending_gradients.end()) {
-//            try_train(gradients->second);
-//            pending_gradients.erase(gradients);
+uint8_t Meta::_n_past_intervals = max_n_past_intervals;
+
+bool LRCache::lookup(SimpleRequest &_req) {
+    static uint64_t i = 0;
+    if (!(i%1000000)) {
+        cout << "mean diff: " << mean_diff << endl;
+        for (int j = 0; j < n_past_intervals; ++j)
+            cout << "weight " << j << ": " << weights[j] << endl;
+        cout << "bias: " << bias << endl;
+    }
+    ++i;
+
+    auto & req = static_cast<AnnotatedRequest &>(_req);
+    auto it = key_map.find(req._id);
+    if (it != key_map.end()) {
+        //add timestamp
+        //update past timestamps
+        bool & list_idx = it->second.first;
+        uint32_t & pos_idx = it->second.second;
+//        auto tmp = meta_holder[list_idx][pos_idx];
+        meta_holder[list_idx][pos_idx].append_past_timestamp(req._t);
+
+        uint64_t _future_timestamp;
+        if (req._t + threshold > req._next_t)
+            _future_timestamp = req._next_t;
+        else
+            _future_timestamp = req._t + threshold;
+        meta_holder[list_idx][pos_idx]._future_timestamp = _future_timestamp;
+
+        return !list_idx;
+        //update future timestamp. Can look only threshold far
+//        auto it = unordered_future_timestamp.find(key);
+//        if (it == unordered_future_timestamp.end()) {
+//            insert
+//            unordered_future_timestamp.insert({{req._id, req._size}, _future_timestamp});
+//            future_timestamp.insert({{req._id, req._size}, _future_timestamp});
+//        } else {
+//            if has pending training data, put it
+//            auto gradients = pending_gradients.find(req._t);
+//            if (gradients != pending_gradients.end()) {
+//                try_train(gradients->second);
+//                pending_gradients.erase(gradients);
+//            }
+//            replace
+//            it->second = _future_timestamp;
+//            future_timestamp.left.replace_data(future_timestamp.left.find(key), _future_timestamp);
 //        }
-//        //replace
-//        it->second = _future_timestamp;
-//        future_timestamp.left.replace_data(future_timestamp.left.find(key), _future_timestamp);
-//    }
-//
+
+    }
+
 //    try_gc(req._t);
-//    static uint64_t i = 0;
-//    if (!(i%1000000)) {
-//        cout << "mean diff: " << mean_diff << endl;
-//        for (int j = 0; j < n_past_intervals; ++j)
-//            cout << "weight " << j << ": " << weights[j] << endl;
-//        cout << "bias: " << bias << endl;
-//    }
-//    ++i;
+
 //    return RandomCache::lookup(req);
-//}
-//
+}
+
 //void LRCache::try_gc(uint64_t t) {
 //    /*
 //     * objects that never request again
@@ -103,73 +111,140 @@ void RandomCache::evict() {
 //            break;
 //    }
 //}
-//
-//void LRCache::admit(SimpleRequest &_req) {
-//    AnnotatedRequest & req = static_cast<AnnotatedRequest &>(_req);
-//    const uint64_t size = req.get_size();
-//    // object feasible to store?
-//    if (size > _cacheSize) {
-//        LOG("L", _cacheSize, req.get_id(), size);
-//        return;
-//    }
-//    // admit new object
-//    key_space.insert({req.get_id(), req.get_size()});
-//    _currentSize += size;
-//
-//    // check eviction needed
-//    while (_currentSize > _cacheSize) {
-//        evict(req._t);
-//    }
-//}
-//
-//void LRCache::evict(uint64_t t) {
-//    static double * past_intervals;
-//    static double future_interval;
-//    static double max_future_interval;
-//    static pair<uint64_t, uint64_t> max_key;
-////    static uint64_t tmp_i = 0;
-//
-//    if (past_intervals == nullptr)
-//        past_intervals = new double[n_past_intervals];
-//
-//    for (int i = 0; i < sample_rate; i++) {
-//        const auto & key = key_space.pickRandom();
-//        //fill in past_interval
-//        const auto & _past_timestamps = past_timestamps.find({key.first, key.second})->second;
-//        {
-//            int j = 0;
-//            for (auto &it: _past_timestamps) {
-//                uint64_t past_interval = t - it;
-//                if (past_interval >= threshold)
-//                    past_intervals[j] = log1p_threshold;
-//                else
-//                    past_intervals[j] = log1p(past_interval);
-//                j++;
-//            }
-//            for (; j < n_past_intervals; j++)
-//                past_intervals[j] = log1p_threshold;
-//        }
-//
-//        future_interval = 0;
-//        for (int j = 0; j < n_past_intervals; j++)
-//            future_interval += weights[j] * past_intervals[j];
-//
-//        if (!i || future_interval > max_future_interval) {
-//            max_future_interval = future_interval;
-//            max_key = key;
-//        }
-//
-//        //append training data
+
+void LRCache::admit(SimpleRequest &_req) {
+    AnnotatedRequest & req = static_cast<AnnotatedRequest &>(_req);
+    const uint64_t & size = req._size;
+    // object feasible to store?
+    if (size > _cacheSize) {
+        LOG("L", _cacheSize, req.get_id(), size);
+        return;
+    }
+
+    if (size + _currentSize <= _cacheSize) {
+        //never evict
+        auto it = key_map.find(req._id);
+        if (it == key_map.end()) {
+            //fresh insert
+            key_map.insert({req._id, {0, (uint32_t) meta_holder[0].size()}});
+            uint64_t _future_timestamp;
+            if (req._t + threshold > req._next_t)
+                _future_timestamp = req._next_t;
+            else
+                _future_timestamp = req._t + threshold;
+            meta_holder[0].emplace_back(req._id, req._size, req._t, _future_timestamp);
+            _currentSize += size;
+            return;
+        } else {
+            //bring list 1 to list 0
+            uint32_t new_pos = meta_holder[0].size();
+            meta_holder[0].emplace_back(meta_holder[1][it->second.second]);
+            uint32_t deactivate_tail_pos = meta_holder[1].size()-1;
+            if (it->second.second !=  deactivate_tail_pos) {
+                //swap tail
+                meta_holder[1][it->second.second] = meta_holder[1][deactivate_tail_pos];
+                key_map.find(meta_holder[1][deactivate_tail_pos]._key)->second.second = it->second.second;
+            }
+            meta_holder[1].pop_back();
+            it->second.first = 0;
+            it->second.second = new_pos;
+            _currentSize += size;
+            return;
+        }
+    }
+
+    auto it = key_map.find(req._id);
+    if (it == key_map.end()) {
+        //fresh insert
+        key_map.insert({req._id, {0, (uint32_t) meta_holder[0].size()}});
+        uint64_t _future_timestamp;
+        if (req._t + threshold > req._next_t)
+            _future_timestamp = req._next_t;
+        else
+            _future_timestamp = req._t + threshold;
+        meta_holder[0].emplace_back(req._id, req._size, req._t, _future_timestamp);
+        _currentSize += size;
+    } else {
+        //insert-evict
+        auto epair = rank(req._t);
+        auto & key0 = epair.first;
+        auto & pos0 = epair.second;
+        auto & key1 = req._id;
+        auto & pos1 = it->second.second;
+        _currentSize = _currentSize - meta_holder[0][pos0]._size + req._size;
+        swap(meta_holder[0][pos0], meta_holder[1][pos1]);
+        it->second.first = 0;
+        it->second.second = pos0;
+        auto iit = key_map.find(key0);
+        iit->second.first = 1;
+        iit->second.second = pos1;
+    }
+    // check eviction needed
+    while (_currentSize > _cacheSize) {
+        evict(req._t);
+    }
+
+}
+
+
+pair<uint64_t, uint32_t> LRCache::rank(const uint64_t & t) {
+    double past_intervals[max_n_past_intervals];
+    double future_interval;
+    double max_future_interval;
+    uint64_t max_key;
+    uint32_t max_pos;
+    uint32_t i;
+    uint8_t j;
+    uint8_t past_timestamp_idx;
+    uint64_t past_interval;
+
+//    static uint64_t tmp_i = 0;
+    uint32_t rand_idx = _distribution(_generator) % meta_holder[0].size();
+    uint8_t n_sample;
+    if (sample_rate < meta_holder[0].size())
+        n_sample = sample_rate;
+    else
+        n_sample = meta_holder[0].size();
+
+    for (i = 0; i < n_sample; i++) {
+        uint32_t pos = (i+rand_idx)%meta_holder[0].size();
+        auto & meta = meta_holder[0][pos];
+//        assert(meta._size == 1);
+//        auto it = key_map.find(meta._key);
+//        assert(it != key_map.end());
+        //fill in past_interval
+        for (j = 0; j < meta._past_timestamp_idx && j < n_past_intervals; ++j) {
+            past_timestamp_idx = (meta._past_timestamp_idx - 1 - j) % n_past_intervals;
+            past_interval = t - meta._past_timestamps[past_timestamp_idx];
+            if (past_interval >= threshold)
+                past_intervals[j] = log1p_threshold;
+            else
+                past_intervals[j] = log1p(past_interval);
+        }
+        for (; j < n_past_intervals; j++)
+            past_intervals[j] = log1p_threshold;
+
+        future_interval = 0;
+        for (j = 0; j < n_past_intervals; j++)
+            future_interval += weights[j] * past_intervals[j];
+
+        if (!i || future_interval > max_future_interval) {
+            max_future_interval = future_interval;
+            max_key = meta._key;
+            max_pos = pos;
+        }
+
+        //append training data
 //        uint64_t training_idx = unordered_future_timestamp.find(key)->second;
 //        double diff = future_interval+bias - log1p(training_idx-t);
 //        mean_diff = 0.99*mean_diff + 0.01*abs(diff);
-////        cout<<mean_diff<<endl;
-////        if (!(tmp_i%100000)) {
-////            cout<<past_timestamps.size()<<endl;
-////            ++tmp_i;
-////
-////        }
+//        cout<<mean_diff<<endl;
+//        if (!(tmp_i%100000)) {
+//            cout<<past_timestamps.size()<<endl;
+//            ++tmp_i;
 //
+//        }
+
 //        auto _pending_gradients = pending_gradients.find(training_idx);
 //        if (_pending_gradients == pending_gradients.end()) {
 //            auto gradients = new double[n_past_intervals+2];
@@ -185,12 +260,35 @@ void RandomCache::evict() {
 //            gradients[n_past_intervals] += diff;
 //            gradients[n_past_intervals+1] += 1;
 //        }
-//    }
-//
+    }
+    return {max_key, max_pos};
 //    key_space.erase(max_key);
 //    _currentSize -= max_key.second;
-//}
-//
+}
+
+void LRCache::evict(const uint64_t & t) {
+    auto epair = rank(t);
+    uint64_t & key = epair.first;
+    uint32_t & old_pos = epair.second;
+
+    //bring list 0 to list 1
+    uint32_t new_pos = meta_holder[1].size();
+
+    meta_holder[1].emplace_back(meta_holder[0][old_pos]);
+    uint32_t activate_tail_idx = meta_holder[0].size()-1;
+    if (old_pos !=  activate_tail_idx) {
+        //move tail
+        meta_holder[0][old_pos] = meta_holder[0][activate_tail_idx];
+        key_map.find(meta_holder[0][activate_tail_idx]._key)->second.second = old_pos;
+    }
+    meta_holder[0].pop_back();
+
+    auto it = key_map.find(key);
+    it->second.first = 1;
+    it->second.second = new_pos;
+    _currentSize -= meta_holder[1][new_pos]._size;
+}
+
 //void LRCache::try_train(double * gradients) {
 //    //train
 //    static double * weight_update;
