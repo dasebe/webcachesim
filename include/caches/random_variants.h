beeda8d989f880edabc0c6d68ef895dcae5058e8
zhenyus <sunnyszy@gmail.com>
2018-12-17 14:21:35 -0500

    refactor: move lruk_sample out of a new file

--- include/caches/random_variants.h
+++ include/caches/random_variants.h
@@ -169,56 +169,6 @@ static Factory<LRCache> factoryLR("LR");
 
 
 
-class LRUKSampleCache : public Cache
-{
-public:
-    unordered_map<uint64_t, pair<bool, uint32_t>> key_map;
-    vector<LRMeta> meta_holder[2];
 
-    uint sample_rate = 32;
-    // n_past_interval
-    uint8_t n_past_intervals = 4;
-    bool forget_on_evict = true;
 
-    //todo: seed and generator
-    default_random_engine _generator = default_random_engine();
-    uniform_int_distribution<std::size_t> _distribution = uniform_int_distribution<std::size_t>();
-
-    LRUKSampleCache()
-        : Cache()
-    {
-    }
-
-    void init_with_params(map<string, string> params) override {
-        //set params
-        for (auto& it: params) {
-            if (it.first == "sample_rate") {
-                sample_rate = stoul(it.second);
-            } else if (it.first == "n_past_intervals") {
-                n_past_intervals = (uint8_t) stoi(it.second);
-            } else if (it.first == "forget_on_evict") {
-                forget_on_evict = (bool) stoi(it.second);
-            } else {
-                cerr << "unrecognized parameter: " << it.first << endl;
-            }
-        }
-
-        //init
-        if (n_past_intervals > max_n_past_intervals) {
-            cerr << "error: n_past_intervals exceeds max limitation: " << max_n_past_intervals << endl;
-            assert(false);
-        }
-        LRMeta::_n_past_intervals = n_past_intervals;
-    }
-
-    virtual bool lookup(SimpleRequest& req);
-    virtual void admit(SimpleRequest& req);
-    virtual void evict(const uint64_t & t);
-    void evict(SimpleRequest & req) {};
-    void evict() {};
-    //sample, rank the 1st and return
-    pair<uint64_t, uint32_t > rank(const uint64_t & t);
-};
-
-static Factory<LRUKSampleCache> factoryLRUKSample("LRUKSample");
 #endif //WEBCACHESIM_RANDOM_VARIANTS_H
