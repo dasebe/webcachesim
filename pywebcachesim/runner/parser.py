fa20476e6fcd914d0f2d792a67c98034eeae0528
Zhenyu Song <sunnyszy@gmail.com>
2019-01-31 17:49:40 +0800

    refactor: simply input config, and support trace specific algorithm params

--- pywebcachesim/runner/parser.py
+++ pywebcachesim/runner/parser.py
@@ -1,7 +1,6 @@
 import argparse
 import yaml
 
-
 scheduler_args_default = {
     'debug': False
 }
@@ -9,34 +8,35 @@ scheduler_args_default = {
 
 def parse_cmd_args():
     # how to schedule parallel simulations
-    scheduler_parser = argparse.ArgumentParser()
-    scheduler_parser.add_argument('--debug',
-                                  help='debug mode only run 1 task locally',
-                                  type=bool,
-                                  choices=[True, False])
-    scheduler_parser.add_argument('--config_file', type=str, nargs='?', help='runner configuration file')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--debug',
+                        help='debug mode only run 1 task locally',
+                        type=bool,
+                        choices=[True, False])
+    parser.add_argument('--config_file', type=str, nargs='?', help='runner configuration file')
+    parser.add_argument('--algorithm_param_file', type=str, help='algorithm parameter config file')
+    # parser.add_argument('--trace_file', type=str, nargs='+', help='path to trace file')
+    # parser.add_argument('--cache_type', type=str, nargs='+', help='cache algorithms')
+    # parser.add_argument('--cache_size', type=int, nargs='+', help='cache size in terms of byte')
+    args = parser.parse_args()
+    # worker_parser.add_argument('--n_warmup', type=int, help='number of requests to warmup the cache')
+    # worker_parser.add_argument('--uni_size', type=int, help='whether assume each object has same size of 1')
     # read from PATH, WEBCACHESIM_TRACE_DIR
     # scheduler_parser.add_argument('--trace_dir',
     #                               type=str,
     #                               nargs='?',
     #                               help='whether the trace is placed')
-    scheduler_parser.add_argument('--algorithm_param_file', type=str, help='algorithm parameter config file')
-    scheduler_args, unknown_args = scheduler_parser.parse_known_args()
+    # scheduler_args, unknown_args = scheduler_parser.parse_known_args()
 
     # args that affects the result
-    worker_parser = argparse.ArgumentParser()
-    worker_parser.add_argument('--trace_files', type=str, nargs='+', help='path to trace file')
-    worker_parser.add_argument('--cache_types', type=str, nargs='+', help='cache algorithms')
-    worker_parser.add_argument('--cache_sizes', type=int, nargs='+', help='cache size in terms of byte')
-    worker_parser.add_argument('--n_warmup', type=int, help='number of requests to warmup the cache')
-    worker_parser.add_argument('--uni_size', type=int, help='whether assume each object has same size of 1')
-    worker_args, unknown_args = worker_parser.parse_known_args(unknown_args)
+    # worker_parser = argparse.ArgumentParser()
+    # worker_args, unknown_args = worker_parser.parse_known_args(unknown_args)
 
     # args that don't affects the result
-    worker_extra_parser = argparse.ArgumentParser()
-    worker_extra_parser.add_argument('--segment_window', type=int, help='interval to record segment hit rate')
-    scheduler_parser.add_argument('--dburl', type=str)
-    worker_extra_args = worker_extra_parser.parse_args(unknown_args)
+    # worker_extra_parser = argparse.ArgumentParser()
+    # worker_extra_parser.add_argument('--segment_window', type=int, help='interval to record segment hit rate')
+    # scheduler_parser.add_argument('--dburl', type=str)
+    # worker_extra_args = worker_extra_parser.parse_args(unknown_args)
 
     # parser.add_argument('--n_early_stop',
     #                     type=int,
@@ -48,7 +48,7 @@ def parse_cmd_args():
     #                     action='store_true',
     #                     help='whether write tensorboard summary')
 
-    return vars(scheduler_args), vars(worker_args), vars(worker_extra_args)
+    return vars(args)
 
 
 def extend_params(param: dict):
@@ -75,68 +75,48 @@ def extend_params(param: dict):
     return res
 
 
-def job_to_tasks(scheduler_args, worker_args, worker_extra_args):
+def job_to_tasks(args):
     """
     convert job config to list of task
     @:returns dict/[dict]
     """
     # job config file
-    if scheduler_args.get('config_file') is not None:
-        with open(scheduler_args['config_file']) as f:
-            file_params = yaml.load(f)
-        if 'scheduler_args' in file_params:
-            for k, v in file_params['scheduler_args'].items():
-                av = scheduler_args.get(k)
-                if av is None:
-                    scheduler_args[k] = v
-
-        if 'worker_args' in file_params:
-            for k, v in file_params['worker_args'].items():
-                av = worker_args.get(k)
-                if av is None:
-                    worker_args[k] = v
-
-        if 'worker_extra_args' in file_params:
-            for k, v in file_params['worker_extra_args'].items():
-                av = worker_extra_args.get(k)
-                if av is None:
-                    worker_extra_args[k] = v
-
-    # set default value
-    for k, v in scheduler_args_default.items():
-        if scheduler_args.get(k) is None:
-            scheduler_args[k] = v
+    assert args.get('config_file') is not None
+    with open(args['config_file']) as f:
+        file_params = yaml.load(f)
+    for k, v in file_params.items():
+        if args.get(k) is None:
+            args[k] = v
 
     # load algorithm parameters
-    algorithm_params = {}
-    if scheduler_args.get('algorithm_param_file') is not None:
-        with open(scheduler_args['algorithm_param_file']) as f:
-            algorithm_params = yaml.load(f)
-        for alg in algorithm_params:
-            algorithm_params[alg] = extend_params(algorithm_params[alg])
+    assert args.get('algorithm_param_file') is not None
+    with open(args['algorithm_param_file']) as f:
+        algorithm_params = yaml.load(f)
+    for alg in algorithm_params:
+        if alg in args:
+            args[alg] = {**algorithm_params[alg], **args[alg]}
+        else:
+            args[alg] = algorithm_params[alg]
+        args[alg] = extend_params(args[alg])
 
     tasks = []
-    for trace_file in worker_args['trace_files']:
-        for cache_type in worker_args['cache_types']:
-            if worker_args.get("cache_sizes") is not None:
-                cache_sizes = worker_args["cache_sizes"]
-            else:
-                cache_sizes = worker_args["trace_files"][trace_file]
-            for cache_size in cache_sizes:
-                parameters_list = [{}] if algorithm_params.get(cache_type) is None else \
-                    algorithm_params[cache_type]
-                for parameters in parameters_list:
-                    task = {
-                        'trace_file': trace_file,
-                        'cache_type': cache_type,
-                        'cache_size': cache_size,
-                        **parameters,
-                    }
-                    for k, v in worker_args.items():
-                        if k not in ['cache_sizes', 'cache_types', 'trace_files'] and v is not None:
-                            task[k] = v
-                    tasks.append(task)
-    return scheduler_args, tasks, worker_extra_args
+    for cache_type in args['cache_type']:
+        for cache_size in args['cache_size']:
+            parameters_list = [{}] if args.get(cache_type) is None else \
+                args[cache_type]
+            for parameters in parameters_list:
+                task = {
+                    'trace_file': args['trace_file'],
+                    'cache_type': cache_type,
+                    'cache_size': cache_size,
+                    **parameters,
+                }
+                for k, v in args.items():
+                    if k not in ['cache_size', 'cache_type', 'trace_file'] and k not in algorithm_params and \
+                            v is not None:
+                        task[k] = v
+                tasks.append(task)
+    return tasks
 
 
 def parse():
@@ -145,10 +125,8 @@ def parse():
     @:return: parsed nest dict
     """
 
-    scheduler_args, worker_args, worker_extra_args = parse_cmd_args()
-    scheduler_args, tasks, worker_extra_args = job_to_tasks(scheduler_args, worker_args, worker_extra_args)
-    if scheduler_args["debug"]:
-        print(scheduler_args)
+    args = parse_cmd_args()
+    tasks = job_to_tasks(args)
+    if args["debug"]:
         print(tasks)
-        print(worker_extra_args)
-    return scheduler_args, tasks, worker_extra_args
+    return args, tasks
--- pywebcachesim/runner/runner.py
+++ pywebcachesim/runner/runner.py
@@ -3,20 +3,14 @@ import time
 import subprocess
 
 
-def to_task_str(scheduler_args: dict, task: dict, worker_extra_args: dict):
+def to_task_str(task: dict):
     """
     split deterministic args and nodeterminstics args. Add _ prefix to later
     """
 
     params = {}
     for k, v in task.items():
-        if k not in ['trace_file', 'cache_type', 'cache_size']:
-            params[k] = str(v)
-    for k, v in worker_extra_args.items():
-        if v is not None:
-            params[k] = str(v)
-    for k, v in scheduler_args.items():
-        if k not in ['debug', 'config_file', 'algorithm_param_file'] and v is not None:
+        if k not in ['debug', 'config_file', 'algorithm_param_file', 'trace_file', 'cache_type', 'cache_size'] and v is not None:
             params[k] = str(v)
     params = [f'{k} {v}'for k, v in params.items()]
     params = ' '.join(params)
@@ -24,9 +18,9 @@ def to_task_str(scheduler_args: dict, task: dict, worker_extra_args: dict):
     return res
 
 
-def runner_run(scheduler_args: dict, tasks: list, worker_extra_args: dict):
+def runner_run(args: dict, tasks: list):
     # debug mode, only 1 task
-    if scheduler_args["debug"]:
+    if args["debug"]:
         tasks = tasks[:1]
 
     ts = int(time.time())
@@ -34,7 +28,7 @@ def runner_run(scheduler_args: dict, tasks: list, worker_extra_args: dict):
           f'generating job file to /tmp/{ts}.job')
     with open(f'/tmp/{ts}.job', 'w') as f:
         for i, task in enumerate(tasks):
-            task_str = to_task_str(scheduler_args, task, worker_extra_args)
+            task_str = to_task_str(task)
             # f.write(task_str+f' &> /tmp/{ts}.log\n')
             ts_task = int(time.time()*1000000)
             task_str = f'bash --login -c "{task_str}" &> /tmp/{ts_task}.log\n'
@@ -48,8 +42,8 @@ def runner_run(scheduler_args: dict, tasks: list, worker_extra_args: dict):
 
 
 def main():
-    scheduler_args, tasks, worker_extra_args = parser.parse()
-    return runner_run(scheduler_args, tasks, worker_extra_args)
+    args, tasks = parser.parse()
+    return runner_run(args, tasks)
 
 
 if __name__ == '__main__':
