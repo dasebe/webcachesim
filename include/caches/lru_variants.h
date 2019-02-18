32eb0260612ddc6af7cfbff57ca0fb9ca860c8be
zhenyus <sunnyszy@gmail.com>
2019-02-18 09:34:12 -0500

    feat: add adaptsize

--- include/caches/lru_variants.h
+++ include/caches/lru_variants.h
@@ -3,8 +3,10 @@
 
 #include <unordered_map>
 #include <list>
+#include <random>
 #include "cache.h"
 #include "cache_object.h"
+#include "adaptsize_const.h" /* AdaptSize constants */
 
 typedef std::list<CacheObject>::iterator ListIteratorType;
 typedef std::unordered_map<CacheObject, ListIteratorType> lruCacheMapType;
@@ -107,6 +109,53 @@ static Factory<FIFOCache> factoryFIFO("FIFO");
 //};
 //
 //static Factory<FilterCache> factoryFilter("Filter");
+
+/*
+  AdaptSize: ExpLRU with automatic adaption of the _cParam
+*/
+class AdaptSizeCache : public LRUCache
+{
+public:
+    AdaptSizeCache();
+    virtual ~AdaptSizeCache()
+    {
+    }
+
+    virtual void setPar(std::string parName, std::string parValue);
+    virtual bool lookup(SimpleRequest&);
+    virtual void admit(SimpleRequest&);
+
+private:
+    double _cParam; //
+    uint64_t statSize;
+    uint64_t _maxIterations;
+    uint64_t _reconfiguration_interval;
+    uint64_t _nextReconfiguration;
+    double _gss_v;  // golden section search book parameters
+    // for random number generation
+    std::uniform_real_distribution<double> _uniform_real_distribution =
+            std::uniform_real_distribution<double>(0.0, 1.0);
+
+    struct ObjInfo {
+        double requestCount; // requestRate in adaptsize_stub.h
+        uint64_t objSize;
+
+        ObjInfo() : requestCount(0.0), objSize(0) { }
+    };
+    std::unordered_map<CacheObject, ObjInfo> _longTermMetadata;
+    std::unordered_map<CacheObject, ObjInfo> _intervalMetadata;
+
+    void reconfigure();
+    double modelHitRate(double c);
+
+    // align data for vectorization
+    std::vector<double> _alignedReqCount;
+    std::vector<double> _alignedObjSize;
+    std::vector<double> _alignedAdmProb;
+};
+
+static Factory<AdaptSizeCache> factoryAdaptSize("AdaptSize");
+
 
 /*
   S4LRU
