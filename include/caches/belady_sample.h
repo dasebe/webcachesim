9d8e2cce5afc66da15fa72f54c15186553d00bb3
zhenyus <sunnyszy@gmail.com>
2019-03-19 18:54:24 -0400

    feat: choose future information based on req-req time

--- include/caches/belady_sample.h
+++ include/caches/belady_sample.h
@@ -7,25 +7,44 @@
 
 #include <cache.h>
 #include <unordered_map>
-#include "lr.h"
+#include <cmath>
+#include <random>
 
 using namespace std;
+
+class BeladySampleMeta {
+public:
+    uint64_t _key;
+    uint64_t _size;
+    uint64_t _past_timestamp;
+    uint64_t _future_timestamp;
+
+    BeladySampleMeta(const uint64_t & key, const uint64_t & size, const uint64_t & past_timestamp,
+                     const uint64_t & future_timestamp) {
+        _key = key;
+        _size = size;
+        _past_timestamp = past_timestamp;
+        _future_timestamp = future_timestamp;
+    }
+
+    inline void update(const uint64_t &past_timestamp, const uint64_t &future_timestamp) {
+        _past_timestamp = past_timestamp;
+        _future_timestamp = future_timestamp;
+    }
+};
+
 
 class BeladySampleCache : public Cache
 {
 public:
     //key -> (0/1 list, idx)
     unordered_map<uint64_t, pair<bool, uint32_t>> key_map;
-    vector<LRMeta> meta_holder[2];
+    vector<BeladySampleMeta> meta_holder[2];
 
     // sample_size
     uint sample_rate = 32;
     // threshold
     uint64_t threshold = 10000000;
-    double log1p_threshold = log1p(threshold);
-    // n_past_interval
-    uint8_t n_past_intervals = 4;
-    uint64_t evicted_f = 0;
 
     //todo: seed and generator
     default_random_engine _generator = default_random_engine();
@@ -43,14 +62,10 @@ public:
                 sample_rate = stoul(it.second);
             } else if (it.first == "threshold") {
                 threshold = stoull(it.second);
-                log1p_threshold = std::log1p(threshold);
-            } else if (it.first == "n_past_intervals") {
-                n_past_intervals = (uint8_t) stoi(it.second);
             } else {
                 cerr << "unrecognized parameter: " << it.first << endl;
             }
         }
-        LRMeta::_n_past_intervals = n_past_intervals;
     }
 
     virtual bool lookup(SimpleRequest& req);
@@ -60,51 +75,50 @@ public:
     void evict() {};
     //sample, rank the 1st and return
     pair<uint64_t, uint32_t > rank(const uint64_t & t);
-    void sample(uint64_t &t);
 };
 
 static Factory<BeladySampleCache> factoryBeladySample("BeladySample");
 
 
-class BeladySampleCacheFilter : public BeladySampleCache
-{
-public:
-    bool out_sample = true;
-    double mean_diff=0;
-    uint64_t n_window_bins = 10;
-    uint64_t size_bin;
-    uint64_t gradient_window = 10000;
-
-    BeladySampleCacheFilter()
-        : BeladySampleCache()
-    {
-    }
-
-    void init_with_params(map<string, string> params) override {
-        BeladySampleCache::init_with_params(params);
-        //set params
-        for (auto& it: params) {
-            if (it.first == "out_sample") {
-                out_sample = (bool) stoul(it.second);
-            } else if (it.first == "gradient_window") {
-                gradient_window = stoull(it.second);
-            } else if (it.first == "n_window_bins") {
-                n_window_bins = stoull(it.second);
-            } else {
-                cerr << "unrecognized parameter: " << it.first << endl;
-            }
-        }
-        size_bin = threshold/n_window_bins;
-    }
-
-    bool lookup(SimpleRequest &_req, vector<vector<Gradient>> & ext_pending_gradients,
-                vector<double> & ext_weights, double & ext_bias);
-    void sample(uint64_t &t, vector<vector<Gradient>> & ext_pending_gradients,
-                vector<double> & ext_weights, double & ext_bias);
-
-};
-
-static Factory<BeladySampleCacheFilter> factoryBeladySampleFilter("BeladySampleFilter");
+//class BeladySampleCacheFilter : public BeladySampleCache
+//{
+//public:
+//    bool out_sample = true;
+//    double mean_diff=0;
+//    uint64_t n_window_bins = 10;
+//    uint64_t size_bin;
+//    uint64_t gradient_window = 10000;
+//
+//    BeladySampleCacheFilter()
+//        : BeladySampleCache()
+//    {
+//    }
+//
+//    void init_with_params(map<string, string> params) override {
+//        BeladySampleCache::init_with_params(params);
+//        //set params
+//        for (auto& it: params) {
+//            if (it.first == "out_sample") {
+//                out_sample = (bool) stoul(it.second);
+//            } else if (it.first == "gradient_window") {
+//                gradient_window = stoull(it.second);
+//            } else if (it.first == "n_window_bins") {
+//                n_window_bins = stoull(it.second);
+//            } else {
+//                cerr << "unrecognized parameter: " << it.first << endl;
+//            }
+//        }
+//        size_bin = threshold/n_window_bins;
+//    }
+//
+//    bool lookup(SimpleRequest &_req, vector<vector<Gradient>> & ext_pending_gradients,
+//                vector<double> & ext_weights, double & ext_bias);
+//    void sample(uint64_t &t, vector<vector<Gradient>> & ext_pending_gradients,
+//                vector<double> & ext_weights, double & ext_bias);
+//
+//};
+//
+//static Factory<BeladySampleCacheFilter> factoryBeladySampleFilter("BeladySampleFilter");
 
 
 
--- src/caches/belady_sample.cpp
+++ src/caches/belady_sample.cpp
@@ -8,308 +8,9 @@
 using namespace std;
 
 
-void BeladySampleCache::sample(uint64_t &t) {
-    if (meta_holder[0].empty() || meta_holder[1].empty())
-        return;
-#ifdef LOG_SAMPLE_RATE
-    bool log_flag = ((double) rand() / (RAND_MAX)) < LOG_SAMPLE_RATE;
-    //sample meta
-    if (log_flag) {
-        cout << -1 << " "<<t<<" "<<evicted_f<<endl;
-    }
-
-    //sample list 0
-    if (log_flag) {
-        uint32_t rand_idx = _distribution(_generator) % meta_holder[0].size();
-        uint n_sample = min( (uint) ceil((double) sample_rate*meta_holder[0].size()/(meta_holder[0].size()+meta_holder[1].size())),
-                             (uint) meta_holder[0].size());
-
-        for (uint32_t i = 0; i < n_sample; i++) {
-            uint32_t pos = (i + rand_idx) % meta_holder[0].size();
-            auto &meta = meta_holder[0][pos];
-
-            //fill in past_interval
-            uint8_t j = 0;
-            auto past_intervals = vector<uint64_t >(n_past_intervals);
-            for (j = 0; j < meta._past_timestamp_idx && j < n_past_intervals; ++j) {
-                uint8_t past_timestamp_idx = (meta._past_timestamp_idx - 1 - j) % n_past_intervals;
-                uint64_t past_interval = t - meta._past_timestamps[past_timestamp_idx];
-                if (past_interval >= threshold)
-                    past_intervals[j] = threshold;
-                else
-                    past_intervals[j] = past_interval;
-            }
-            for (; j < n_past_intervals; j++)
-                past_intervals[j] = threshold;
-
-
-            uint64_t known_future_interval;
-            //known_future_interval < threshold
-            if (meta._future_timestamp - t < threshold) {
-                known_future_interval = meta._future_timestamp - t;
-            }
-            else {
-                known_future_interval = threshold-1;
-            }
-
-            //print distribution
-            cout << 0 <<" ";
-            for (uint k = 0; k < n_past_intervals; ++k)
-                cout << past_intervals[k] << " ";
-            cout << known_future_interval << endl;
-        }
-    }
-#endif
-
-    //sample list 1, but don't update
-    {
-        uint32_t rand_idx = _distribution(_generator) % meta_holder[1].size();
-        //sample less from list 1 as there are gc
-        uint n_sample = min( (uint) floor( (double) sample_rate*meta_holder[1].size()/(meta_holder[0].size()+meta_holder[1].size())),
-                             (uint) meta_holder[1].size());
-//        cout<<n_sample<<endl;
-
-        for (uint32_t i = 0; i < n_sample; i++) {
-            uint32_t pos;
-            //garbage collection
-            while (true) {
-                if (meta_holder[1].empty())
-                    return;
-                pos = static_cast<uint32_t >((i + rand_idx) % meta_holder[1].size());
-                auto &meta = meta_holder[1][pos];
-                uint8_t oldest_idx = (meta._past_timestamp_idx - (uint8_t) 1)%n_past_intervals;
-                uint64_t & past_timestamp = meta._past_timestamps[oldest_idx];
-                if (past_timestamp + threshold < t) {
-                    uint64_t & ekey = meta._key;
-                    key_map.erase(ekey);
-                    //evict
-                    uint32_t tail1_pos = meta_holder[1].size()-1;
-                    if (pos !=  tail1_pos) {
-                        //swap tail
-                        meta_holder[1][pos] = meta_holder[1][tail1_pos];
-                        key_map.find(meta_holder[1][tail1_pos]._key)->second.second = pos;
-                    }
-                    meta_holder[1].pop_back();
-                } else
-                    break;
-            }
-#ifdef LOG_SAMPLE_RATE
-            if (log_flag) {
-                auto &meta = meta_holder[1][pos];
-                //fill in past_interval
-                uint8_t j = 0;
-                auto past_intervals = vector<uint64_t>(n_past_intervals);
-                for (j = 0; j < meta._past_timestamp_idx && j < n_past_intervals; ++j) {
-                    uint8_t past_timestamp_idx = (meta._past_timestamp_idx - 1 - j) % n_past_intervals;
-                    uint64_t past_interval = t - meta._past_timestamps[past_timestamp_idx];
-                    if (past_interval >= threshold)
-                        past_intervals[j] = threshold;
-                    else
-                        past_intervals[j] = past_interval;
-                }
-                for (; j < n_past_intervals; j++)
-                    past_intervals[j] = threshold;
-
-
-                uint64_t known_future_interval;
-                if (meta._future_timestamp - t < threshold) {
-                    known_future_interval = meta._future_timestamp - t;
-                } else {
-                    known_future_interval = threshold - 1;
-                }
-
-                //print distribution
-                cout << 1 << " ";
-                for (uint k = 0; k < n_past_intervals; ++k)
-                    cout << past_intervals[k] << " ";
-                cout << known_future_interval << endl;
-            }
-#endif
-        }
-    }
-
-}
-
-void BeladySampleCacheFilter::sample(uint64_t &t, vector<vector<Gradient>> & pending_gradients,
-        vector<double> & weights, double & bias) {
-    if (meta_holder[0].empty() || meta_holder[1].empty())
-        return;
-#ifdef LOG_SAMPLE_RATE
-    bool log_flag = ((double) rand() / (RAND_MAX)) < LOG_SAMPLE_RATE;
-#endif
-
-    //sample list 0
-    {
-        uint32_t rand_idx = _distribution(_generator) % meta_holder[0].size();
-        uint n_sample = min( (uint) ceil((double) sample_rate*meta_holder[0].size()/(meta_holder[0].size()+meta_holder[1].size())),
-                             (uint) meta_holder[0].size());
-
-        for (uint32_t i = 0; i < n_sample; i++) {
-            uint32_t pos = (i + rand_idx) % meta_holder[0].size();
-            auto &meta = meta_holder[0][pos];
-
-            //fill in past_interval
-            uint8_t j = 0;
-            auto past_intervals = vector<double >(n_past_intervals);
-            for (j = 0; j < meta._past_timestamp_idx && j < n_past_intervals; ++j) {
-                uint8_t past_timestamp_idx = (meta._past_timestamp_idx - 1 - j) % n_past_intervals;
-                uint64_t past_interval = t - meta._past_timestamps[past_timestamp_idx];
-                if (past_interval >= threshold)
-                    past_intervals[j] = log1p_threshold;
-                else
-                    past_intervals[j] = log1p(past_interval);
-            }
-            for (; j < n_past_intervals; j++)
-                past_intervals[j] = log1p_threshold;
-
-
-            uint64_t known_future_interval;
-            double log1p_known_future_interval;
-            //known_future_interval < threshold
-            if (meta._future_timestamp - t < threshold) {
-                known_future_interval = meta._future_timestamp - t;
-                log1p_known_future_interval = log1p(known_future_interval);
-            }
-            else {
-                known_future_interval = threshold-1;
-                log1p_known_future_interval = log1p_threshold;
-            }
-
-#ifdef LOG_SAMPLE_RATE
-            //print distribution
-            if (log_flag) {
-                cout << 0 <<" ";
-                for (uint k = 0; k < n_past_intervals; ++k)
-                    cout << past_intervals[k] << " ";
-                cout << log1p_known_future_interval << endl;
-            }
-#endif
-
-            double score = 0;
-            for (j = 0; j < n_past_intervals; j++)
-                score += weights[j] * past_intervals[j];
-
-            //statistics
-            double diff = score + bias - log1p_known_future_interval;
-            mean_diff = 0.99 * mean_diff + 0.01 * abs(diff);
-
-            //update gradient
-            auto gradient_window_idx = (t + known_future_interval) / gradient_window;
-            auto bin_idx = known_future_interval / size_bin;
-            if (gradient_window_idx >= pending_gradients.size())
-                pending_gradients.resize(gradient_window_idx + 1);
-            if (pending_gradients[gradient_window_idx].empty())
-                pending_gradients[gradient_window_idx].resize(n_window_bins);
-            auto &gradient = pending_gradients[gradient_window_idx][bin_idx];
-            for (uint k = 0; k < n_past_intervals; ++k)
-                gradient.weights[k] += diff * past_intervals[k];
-            gradient.bias += diff;
-            ++gradient.n_update;
-        }
-    }
-
-    //sample list 1, but don't update
-    {
-        uint32_t rand_idx = _distribution(_generator) % meta_holder[1].size();
-        //sample less from list 1 as there are gc
-        uint n_sample = min( (uint) floor( (double) sample_rate*meta_holder[1].size()/(meta_holder[0].size()+meta_holder[1].size())),
-                             (uint) meta_holder[1].size());
-//        cout<<n_sample<<endl;
-
-        for (uint32_t i = 0; i < n_sample; i++) {
-            //garbage collection
-            while (meta_holder[1].size()) {
-                uint32_t pos = (i + rand_idx) % meta_holder[1].size();
-                auto &meta = meta_holder[1][pos];
-                uint8_t oldest_idx = (meta._past_timestamp_idx - (uint8_t) 1)%n_past_intervals;
-                uint64_t & past_timestamp = meta._past_timestamps[oldest_idx];
-                if (past_timestamp + threshold < t) {
-                    uint64_t & ekey = meta._key;
-                    key_map.erase(ekey);
-                    //evict
-                    uint32_t tail1_pos = meta_holder[1].size()-1;
-                    if (pos !=  tail1_pos) {
-                        //swap tail
-                        meta_holder[1][pos] = meta_holder[1][tail1_pos];
-                        key_map.find(meta_holder[1][tail1_pos]._key)->second.second = pos;
-                    }
-                    meta_holder[1].pop_back();
-                } else
-                    break;
-            }
-            if (!meta_holder[1].size())
-                break;
-            uint32_t pos = (i + rand_idx) % meta_holder[1].size();
-            auto &meta = meta_holder[1][pos];
-            //fill in past_interval
-            uint8_t j = 0;
-            auto past_intervals = vector<double >(n_past_intervals);
-            for (j = 0; j < meta._past_timestamp_idx && j < n_past_intervals; ++j) {
-                uint8_t past_timestamp_idx = (meta._past_timestamp_idx - 1 - j) % n_past_intervals;
-                uint64_t past_interval = t - meta._past_timestamps[past_timestamp_idx];
-                if (past_interval >= threshold)
-                    past_intervals[j] = log1p_threshold;
-                else
-                    past_intervals[j] = log1p(past_interval);
-            }
-            for (; j < n_past_intervals; j++)
-                past_intervals[j] = log1p_threshold;
-
-            uint64_t known_future_interval;
-            double log1p_known_future_interval;
-            if (meta._future_timestamp - t < threshold) {
-                known_future_interval = meta._future_timestamp - t;
-                log1p_known_future_interval = log1p(known_future_interval);
-            }
-            else {
-                known_future_interval = threshold - 1;
-                log1p_known_future_interval = log1p_threshold;
-            }
-
-#ifdef LOG_SAMPLE_RATE
-            //print distribution
-            if (log_flag) {
-                cout << 1 <<" ";
-                for (uint k = 0; k < n_past_intervals; ++k)
-                    cout << past_intervals[k] << " ";
-                cout << log1p_known_future_interval << endl;
-            }
-#endif
-            if (out_sample) {
-                double score = 0;
-                for (j = 0; j < n_past_intervals; j++)
-                    score += weights[j] * past_intervals[j];
-
-                //statistics
-                double diff = score + bias - log1p_known_future_interval;
-                mean_diff = 0.99 * mean_diff + 0.01 * abs(diff);
-
-
-                //update gradient
-                auto gradient_window_idx = (t + known_future_interval) / gradient_window;
-                auto bin_idx = known_future_interval / size_bin;
-                if (gradient_window_idx >= pending_gradients.size())
-                    pending_gradients.resize(gradient_window_idx + 1);
-                if (pending_gradients[gradient_window_idx].empty())
-                    pending_gradients[gradient_window_idx].resize(n_window_bins);
-                auto &gradient = pending_gradients[gradient_window_idx][bin_idx];
-                for (uint k = 0; k < n_past_intervals; ++k)
-                    gradient.weights[k] += diff * past_intervals[k];
-                gradient.bias += diff;
-                ++gradient.n_update;
-            }
-        }
-    }
-}
-
-
 bool BeladySampleCache::lookup(SimpleRequest &_req) {
     auto & req = dynamic_cast<AnnotatedRequest &>(_req);
 
-    //todo: deal with size consistently
-    // belady don't need sample, only filter need
-//    sample(req._t);
-
     auto it = key_map.find(req._id);
     if (it != key_map.end()) {
         //update past timestamps
@@ -321,26 +22,6 @@ bool BeladySampleCache::lookup(SimpleRequest &_req) {
     }
     return false;
 }
-
-bool BeladySampleCacheFilter::lookup(SimpleRequest &_req, vector<vector<Gradient>> & pending_gradients,
-        vector<double >& weights, double & bias) {
-    auto & req = dynamic_cast<AnnotatedRequest &>(_req);
-
-    //todo: deal with size consistently
-    sample(req._t, pending_gradients, weights, bias);
-
-    auto it = key_map.find(req._id);
-    if (it != key_map.end()) {
-        //update past timestamps
-        bool & list_idx = it->second.first;
-        uint32_t & pos_idx = it->second.second;
-        meta_holder[list_idx][pos_idx].update(req._t, req._next_seq);
-
-        return !list_idx;
-    }
-    return false;
-}
-
 
 void BeladySampleCache::admit(SimpleRequest &_req) {
     AnnotatedRequest & req = static_cast<AnnotatedRequest &>(_req);
@@ -408,12 +89,13 @@ pair<uint64_t, uint32_t> BeladySampleCache::rank(const uint64_t & t) {
         uint32_t pos = (i+rand_idx)%meta_holder[0].size();
         auto & meta = meta_holder[0][pos];
         //fill in past_interval
-        uint64_t past_timestamp = meta._past_timestamps[(meta._past_timestamp_idx - 1)%n_past_intervals];
+        uint64_t &past_timestamp = meta._past_timestamp;
 
-        auto future_interval = meta._future_timestamp - t;
-        if (future_interval > threshold) {
-            future_interval = threshold;
-        }
+        uint64_t future_interval;
+        if (meta._future_timestamp - past_timestamp > threshold)
+            future_interval = 2*threshold;
+        else
+            future_interval = meta._future_timestamp - t;
 
         if (!i || future_interval > max_future_interval ||
             ((future_interval == max_future_interval) && (past_timestamp < min_past_timetamp))) {
@@ -422,9 +104,7 @@ pair<uint64_t, uint32_t> BeladySampleCache::rank(const uint64_t & t) {
             max_key = meta._key;
             max_pos = pos;
         }
-
     }
-
     return {max_key, max_pos};
 }
 
