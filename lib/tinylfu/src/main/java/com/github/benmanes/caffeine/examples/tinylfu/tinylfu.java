0de6c8e33945b862e61e98cc5f265a67de9bed31
zhenyus <sunnyszy@gmail.com>
2019-06-09 19:29:12 -0400

    feat: print segment miss/req

--- lib/tinylfu/src/main/java/com/github/benmanes/caffeine/examples/tinylfu/tinylfu.java
+++ lib/tinylfu/src/main/java/com/github/benmanes/caffeine/examples/tinylfu/tinylfu.java
@@ -12,30 +12,21 @@ import java.util.HashMap;
 public class tinylfu {
     public static void main(String[] args) {
         // trace cache_type size k0 v0 k1 v1 ...
-        // byte request = byte hit + byte miss
-        // byte miss = byte eviction + byte in cache
-        // interest_byte_hit = (whole_byte_request - warmup_byte_request) - (whole_byte_evict - warmup_byte_evict) \
-        // - (whole_byte_cache - warmup_byte_cache)
-        // whole_byte_cache ~= warmup_byte_cache
+        // cannot resize dynamically
         String trace_file = args[0];
 
         long cache_size = Long.parseUnsignedLong(args[2]);
-        long n_warmup = 0;
-
+        int segment_window = 10000000;
         for (int i = 3; i < args.length; i += 2) {
-            if (args[i].equals("n_warmup")) {
-                n_warmup = Long.parseUnsignedLong(args[i+1]);
+            if (args[i].equals("segment_window")) {
+                segment_window = Integer.valueOf(args[i + 1]);
             }
         }
 
-
         HashMap<Long, Integer> size_map = new HashMap<>();
 
-//        MetricRegistry registry = new MetricRegistry();
         LoadingCache<Long, Long> cache = Caffeine
                 .newBuilder()
-//                .initialCapacity(cache_size)
-//                .maximumSize(100)
                 .maximumWeight(cache_size)
                 .weigher((k, v) -> size_map.get(k))
                 .recordStats()
@@ -43,57 +34,51 @@ public class tinylfu {
                 .build(k -> k);
 
         BufferedReader reader;
-        long counter = 0;
-        long warmup_n_hit = 0;
-        long warmup_n_request = 0;
-        long whole_byte_request = 0;
-        long warmup_byte_request = 0;
-        long warmup_byte_evict = 0;
+
+        long id;
+        int size;
+        long byte_req = 0, byte_miss = 0, obj_req = 0, obj_miss = 0;
+        long seq = 0;
         try {
             reader = new BufferedReader(new FileReader(trace_file));
             String line = reader.readLine();
             while (line != null) {
-//                System.out.println(line);
-                if (counter == n_warmup) {
-                    warmup_n_hit = cache.stats().hitCount();
-                    warmup_n_request = cache.stats().requestCount();
-                    warmup_byte_evict = cache.stats().evictionWeight();
-                }
                 String[] values = line.split(" ");
-                long key = Long.parseUnsignedLong(values[1]);
-                Integer size = Integer.valueOf(values[2]);
+                id = Long.parseUnsignedLong(values[1]);
+                size = Integer.valueOf(values[2]);
 
-                if (counter < n_warmup)
-                    warmup_byte_request += size;
-                whole_byte_request += size;
+                if (!size_map.containsKey(id))
+                    size_map.put(id, size);
 
-                if (!size_map.containsKey(key)) {
-                    size_map.put(key, size);
+                byte_req += size;
+                obj_req += 1;
+
+                Long v = cache.getIfPresent(id);
+                if (v == null) {
+                    byte_miss += size;
+                    obj_miss += 1;
+                    cache.put(id, id);
                 }
-                cache.get(key);
+
+                seq += 1;
+
+                if (seq%segment_window == 0) {
+                    System.err.println("segment id: " + seq/segment_window);
+                    System.err.println("bmr: " + (double)(byte_miss)/byte_req + "\n");
+                    //spliter
+                    if (seq != segment_window)
+                        System.out.print(" ");
+                    System.out.print(byte_req+" "+byte_miss+" "+obj_req+" "+obj_miss);
+                    byte_req=byte_miss=obj_req=obj_miss=0;
+                }
 
                 // read next line
                 line = reader.readLine();
-                counter += 1;
-                if ((counter % 1000000) == 0) {
-                    System.err.println(counter);
-                }
             }
             reader.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-
-        long whole_n_hit = cache.stats().hitCount();
-        long whole_n_request = cache.stats().requestCount();
-        long whole_byte_evict = cache.stats().evictionWeight();
-        double object_hit_rate = ((double) (whole_n_hit-warmup_n_hit))/(whole_n_request-warmup_n_request);
-        double byte_hit_rate = ((double) (whole_byte_request - warmup_byte_request -
-                (whole_byte_evict - warmup_byte_evict)) )/(whole_byte_request - warmup_byte_request);
-        System.out.println(byte_hit_rate);
-        System.out.println(object_hit_rate);
-//        System.out.println(whole_miss - warmup_miss);
     }
 
 }
--- src/simulation_tinylfu.cpp
+++ src/simulation_tinylfu.cpp
@@ -12,12 +12,14 @@
 #include <string.h>
 #include <boost/algorithm/string.hpp>
 #include <vector>
+#include "nlohmann/json.hpp"
 
 using namespace std;
 using namespace boost;
+using json = nlohmann::json;
 
 string exec(const char* cmd) {
-    std::array<char, 4096> buffer;
+    std::array<char, 4 * 1024 * 1024> buffer = {0};
     std::string result;
     std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
     if (!pipe) {
@@ -34,21 +36,29 @@ using namespace std;
 map<string, string> _simulation_tinylfu(string trace_file, string cache_type, uint64_t cache_size,
                                        map<string, string> params) {
 
-    cerr<<"error: need to refactor as other simulation"<<endl;
-    abort();
-//    string cmd = "java -jar ${WEBCACHESIM_ROOT}/lib/tinylfu/tinylfu.jar " + trace_file + " Adaptive-TinyLFU " \
-//            + to_string(cache_size);
-//    for (auto k: params) {
-//        cmd += " "+k.first+" "+k.second;
-//    }
-//    string r = exec(cmd.c_str());
-//    vector<string> strs;
-//    split(strs, r, is_any_of("\n"));
-//
-//    map<string, string> res = {
-//            {"byte_hit_rate", strs[0]},
-//            {"object_hit_rate", strs[1]},
-//    };
-//
-//    return res;
+    string cmd = "java -jar ${WEBCACHESIM_ROOT}/lib/tinylfu/tinylfu.jar " + trace_file + " Adaptive-TinyLFU " \
+ + to_string(cache_size);
+    for (auto k: params) {
+        cmd += " " + k.first + " " + k.second;
+    }
+    string r = exec(cmd.c_str());
+    vector<string> strs;
+    split(strs, r, is_any_of(" "));
+
+    vector<uint64_t> seg_byte_req, seg_byte_miss, seg_object_req, seg_object_miss;
+    for (int i = 0; i < strs.size(); i += 4) {
+        seg_byte_req.emplace_back(stoull(strs[i]));
+        seg_byte_miss.emplace_back(stoull(strs[i + 1]));
+        seg_object_req.emplace_back(stoull(strs[i + 2]));
+        seg_object_miss.emplace_back(stoull(strs[i + 3]));
+    }
+
+    map<string, string> res = {
+            {"segment_byte_miss",   json(seg_byte_miss).dump()},
+            {"segment_byte_req",    json(seg_byte_req).dump()},
+            {"segment_object_miss", json(seg_object_miss).dump()},
+            {"segment_object_req",  json(seg_object_req).dump()},
+    };
+
+    return res;
 }
