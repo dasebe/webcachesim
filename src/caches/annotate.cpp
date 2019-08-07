b4cb66b37bc005fbe67823048e506a64f111365c
zhenyus <sunnyszy@gmail.com>
2019-08-07 16:37:56 -0400

    improvement: reduce memory overhead of annotation by caching less fields

--- src/caches/annotate.cpp
+++ src/caches/annotate.cpp
@@ -6,35 +6,20 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
-#include <limits>
 #include <unordered_map>
 #include <chrono>
 #include <cstdint>
+#include <cstring>
 
 //max to 4 billion (2**32-1)
 const uint64_t max_next_seq = 0xffffffff;
 using namespace std;
 
-class AnnotatedRequest_
-{
-public:
-    uint64_t _next_seq;
-    uint64_t _t;
-    uint64_t _id; // request object id
-    uint64_t _size; // request size in bytes
-    vector<uint64_t > _extra_features;
-
-    // Create request
-    AnnotatedRequest_(uint64_t id, uint64_t size, uint64_t t, uint64_t next_seq,
-            vector<uint64_t >* extra_features = nullptr)
-            : _id(id), _size(size), _t(t), _next_seq(next_seq) {
-        if (extra_features)
-            _extra_features = *extra_features;
-    }
-};
-
 
 void annotate(string &trace_file, int n_extra_fields) {
+    /*
+     * assume trace is less than max(uint32_t)
+     * */
     auto expect_file = trace_file+".ant";
     ifstream cachefile(expect_file);
     if (cachefile.good()) {
@@ -42,85 +27,87 @@ void annotate(string &trace_file, int n_extra_fields) {
         return;
     }
 
-    // parse trace file
-    vector<AnnotatedRequest_> trace;
-    uint64_t t, id, size;
-    uint64_t i = 0;
+    // in the first path, hold ids; in the reverse path, hold next_seq
+    vector<uint64_t> id_and_next_seq;
 
-    ifstream infile;
-    infile.open(trace_file);
+    uint64_t id;
+    int64_t i = 0;
+    //not actually need t and size
+    uint64_t t, size;
+    vector<uint64_t> extra_features(n_extra_fields, 0);
+
+    ifstream infile(trace_file);
     if (!infile) {
         cerr << "Exception opening/reading annotate original file"<<endl;
         exit(-1);
     }
 
-    vector<uint64_t > extra_features(n_extra_fields, 0);
-
     while(infile>> t >> id >> size) {
         for (int j = 0; j < n_extra_fields; ++j)
-            infile>>extra_features[j];
+            infile >> extra_features[j];
         if (!(++i%1000000))
             cerr<<"reading origin trace: "<<i<<endl;
-        if (n_extra_fields) {
-            //default with infinite future interval
-            trace.emplace_back(id, size, t, max_next_seq, &extra_features);
-        } else {
-            trace.emplace_back(id, size, t, max_next_seq);
-        }
+        id_and_next_seq.emplace_back(id);
     }
 
-    uint64_t totalReqc = trace.size();
-    std::cerr << "scanned trace n=" << totalReqc << std::endl;
-    if (totalReqc > max_next_seq) {
+    uint64_t n_req = id_and_next_seq.size();
+    std::cerr << "scanned trace n=" << n_req << std::endl;
+    if (n_req > max_next_seq) {
         cerr<<"Error: don't support more trace length than "<<max_next_seq<<endl;
-        exit(-1);
+        abort();
     }
 
     // get nextSeen indices
     // assume id has same size
-    i = 0;
-    unordered_map<uint64_t, uint64_t > lastSeen;
-    for (auto it = trace.rbegin(); it != trace.rend(); ++it) {
-        auto lit = lastSeen.find(it->_id);
-        if (lit != lastSeen.end())
-            it->_next_seq = lit->second;
-        lastSeen[it->_id] = totalReqc - i;
-        if (!(++i%1000000))
+    unordered_map<uint64_t, uint32_t> last_seen;
+    for (i = n_req; i >= 0; --i) {
+        uint64_t current_id = id_and_next_seq[i];
+        auto lit = last_seen.find(current_id);
+        if (lit != last_seen.end())
+            id_and_next_seq[i] = lit->second;
+        else
+            id_and_next_seq[i] = max_next_seq;
+        last_seen[current_id] = i;
+        if (!(i % 1000000))
             cerr<<"computing next t: "<<i<<endl;
     }
 
     // get current time
     string now;
     auto timenow = chrono::system_clock::to_time_t(chrono::system_clock::now());
-
-    ofstream outfile;
     auto tmp_file = trace_file + ".ant.tmp." + to_string(timenow);
     cerr<<"writing the annotated trace "<<tmp_file<<endl;
 
-    outfile.open(tmp_file);
+    ofstream outfile(tmp_file);
     if (!outfile) {
         cerr << "Exception opening/reading tmp file"<<endl;
         exit(-1);
     }
 
+    infile.clear();
+    infile.seekg(0, ios::beg);
     //no need to write seq, which is implicit
-    i = 0;
-    for (auto & it: trace) {
-        outfile << it._next_seq << " " << it._t << " " << it._id << " " << it._size;
+    for (i = 0; i < (uint32_t) n_req; ++i) {
+        infile >> t >> id >> size;
         for (int j = 0; j < n_extra_fields; ++j)
-            outfile << " " << it._extra_features[j];
+            infile >> extra_features[j];
+        outfile << id_and_next_seq[i] << " " << t << " " << id << " " << size;
+        for (int j = 0; j < n_extra_fields; ++j)
+            outfile << " " << extra_features[j];
         outfile <<endl;
-        if (!(++i%1000000))
+        if (!(i % 1000000))
             cerr<<"writing: "<<i<<endl;
     }
 
     outfile.close();
 
     //fat has bug in renaming: cross link file system. Actually it's better to do this in python with c wrapper
-    system(("mv "+tmp_file+" "+expect_file).c_str());
-//    if (rename(tmp_file.c_str(), expect_file.c_str())) {
-//        cerr << "Exception in renaming file from "<<tmp_file<<" to "<<expect_file<<" code: "<<strerror(errno)<< endl;
-//        return;
-//    }
+    //as not using /tmp, this is not a problem anymore
+//    system(("mv "+tmp_file+" "+expect_file).c_str());
+    if (rename(tmp_file.c_str(), expect_file.c_str())) {
+        cerr << "Exception in renaming file from " << tmp_file << " to " << expect_file << " code: " << strerror(errno)
+             << endl;
+        return;
+    }
 
 }
