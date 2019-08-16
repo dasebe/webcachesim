967a19ae58eb2d61f7b47e4218322f60260331e9
zhenyus <sunnyszy@gmail.com>
2019-08-15 20:39:24 -0400

    feat: additional collect data from real time

--- src/simulation.cpp
+++ src/simulation.cpp
@@ -33,6 +33,10 @@ using json = nlohmann::json;
 
 map<string, string> _simulation(string trace_file, string cache_type, uint64_t cache_size,
                                 map<string, string> params){
+    if (params.find("n_extra_fields") == params.end()) {
+        cerr << "error: field n_extra_fields is required" << endl;
+        exit(-1);
+    }
     // create cache
     unique_ptr<Cache> webcache = move(Cache::create_unique(cache_type));
     if(webcache == nullptr) {
@@ -45,6 +49,7 @@ map<string, string> _simulation(string trace_file, string cache_type, uint64_t c
 
     bool uni_size = false;
     uint64_t segment_window = 10000000;
+    uint64_t real_time_segment_window = 10000000;
     uint n_extra_fields = 0;
     bool is_metadata_in_cache_size = true;
 
@@ -58,6 +63,9 @@ map<string, string> _simulation(string trace_file, string cache_type, uint64_t c
         } else if (it->first == "segment_window") {
             segment_window = stoull((it->second));
             ++it;
+        } else if (it->first == "real_time_segment_window") {
+            real_time_segment_window = stoull((it->second));
+            it = params.erase(it);
         } else if (it->first == "n_extra_fields") {
             n_extra_fields = stoull(it->second);
             ++it;
@@ -68,8 +76,7 @@ map<string, string> _simulation(string trace_file, string cache_type, uint64_t c
 
     webcache->init_with_params(params);
 
-    ifstream infile;
-    infile.open(trace_file);
+    ifstream infile(trace_file);
     if (!infile) {
         cerr << "Exception opening/reading file"<<endl;
         exit(-1);
@@ -79,23 +86,42 @@ map<string, string> _simulation(string trace_file, string cache_type, uint64_t c
         std::string line;
         getline(infile, line);
         istringstream iss(line);
-        int64_t tmp;
+        uint64_t tmp;
         int counter = 0;
-        while (iss>>tmp) {++counter;}
-        //format: t id size [extra]
-        if (counter != 3+n_extra_fields) {
-            cerr<<"error: input file column should be 3+n_extra_fields"<<endl;
+        while (iss >> tmp) {
+            ++counter;
+        }
+        //todo: check the type of each argument
+        //format: n_seq t id size [extra]
+        if (counter != 3 + n_extra_fields) {
+            cerr << "error: input file column should be 3 + " << n_extra_fields << endl
+                 << "first line: " << line << endl;
             abort();
         }
         infile.clear();
         infile.seekg(0, ios::beg);
     }
-    uint64_t tmp, id, size;
+    uint64_t t, id, size;
     //measure every segment
     uint64_t byte_req = 0, byte_miss = 0, obj_req = 0, obj_miss = 0;
+    //rt: real_time
+    uint64_t rt_byte_req = 0, rt_byte_miss = 0, rt_obj_req = 0, rt_obj_miss = 0;
     //global statistics
     vector<uint64_t> seg_byte_req, seg_byte_miss, seg_object_req, seg_object_miss;
     vector<uint64_t> seg_rss;
+    //rt: real_time
+    vector<uint64_t> rt_seg_byte_req, rt_seg_byte_miss, rt_seg_object_req, rt_seg_object_miss;
+    vector<uint64_t> rt_seg_rss;
+
+    uint64_t time_window_end;
+    {
+        // Zhenyu: not assume t start from any constant, so need to compute the first window
+        infile >> t;
+        time_window_end =
+                real_time_segment_window * (t / real_time_segment_window + (t % real_time_segment_window != 0));
+        infile.clear();
+        infile.seekg(0, ios::beg);
+    }
 
     vector<uint16_t > extra_features(n_extra_fields, 0);
 
@@ -114,7 +140,7 @@ map<string, string> _simulation(string trace_file, string cache_type, uint64_t c
     CacheSizeStatistics size_stat;
 #endif
 
-    while (infile >> tmp >> id >> size) {
+    while (infile >> t >> id >> size) {
         for (int i = 0; i < n_extra_fields; ++i)
             infile>>extra_features[i];
         if (uni_size)
@@ -122,7 +148,56 @@ map<string, string> _simulation(string trace_file, string cache_type, uint64_t c
 
         DPRINTF("seq: %lu\n", seq);
 
+        while (t >= time_window_end) { // in seconds
+            rt_seg_byte_miss.emplace_back(rt_byte_miss);
+            rt_seg_byte_req.emplace_back(rt_byte_req);
+            rt_seg_object_miss.emplace_back(rt_obj_miss);
+            rt_seg_object_req.emplace_back(rt_obj_req);
+            rt_byte_miss = rt_obj_miss = rt_byte_req = rt_obj_req = 0;
+            //real time only read rss info
+            struct proc_t usage;
+            look_up_our_self(&usage);
+            uint64_t metadata_overhead = usage.rss * getpagesize();
+            rt_seg_rss.emplace_back(metadata_overhead);
+            time_window_end += real_time_segment_window;
+        }
+
+        if (seq && !(seq % segment_window)) {
+#ifdef CACHE_SIZE_DECOUPLE
+            if (seq >= n_warmup) {
+                uint32_t snapshot_id = seq/segment_window;
+                cerr<<"snapshoting cache size decoupling at id: "<<snapshot_id<<endl;
+                for (auto &kv: total_request_map) {
+                    if (webcache->has(kv.first)) {
+                        size_stat.update(snapshot_id, kv.second, size_map[kv.first]);
+                    }
+                }
+            }
+#endif
+            auto _t_now = chrono::system_clock::now();
+            cerr << "\nsegment id: " << seq / segment_window - 1 << endl;
+            cerr << "delta t: " << chrono::duration_cast<std::chrono::milliseconds>(_t_now - t_now).count() / 1000.
+                 << endl;
+            t_now = _t_now;
+            cerr << "segment bmr: " << double(byte_miss) / byte_req << endl;
+            seg_byte_miss.emplace_back(byte_miss);
+            seg_byte_req.emplace_back(byte_req);
+            seg_object_miss.emplace_back(obj_miss);
+            seg_object_req.emplace_back(obj_req);
+            byte_miss = obj_miss = byte_req = obj_req = 0;
+            //reduce cache size by metadata
+            struct proc_t usage;
+            look_up_our_self(&usage);
+            uint64_t metadata_overhead = usage.rss * getpagesize();
+            seg_rss.emplace_back(metadata_overhead);
+            if (is_metadata_in_cache_size)
+                webcache->setSize(cache_size - metadata_overhead);
+            cerr << "rss: " << metadata_overhead << endl;
+        }
+
+
         update_metric_req(byte_req, obj_req, size)
+        update_metric_req(rt_byte_req, rt_obj_req, size)
 
 #ifdef MISS_DECOUPLE
         //count total request
@@ -141,6 +216,7 @@ map<string, string> _simulation(string trace_file, string cache_type, uint64_t c
         bool is_hit = webcache->lookup(req);
         if (!is_hit) {
             update_metric_req(byte_miss, obj_miss, size)
+            update_metric_req(rt_byte_miss, rt_obj_miss, size)
             webcache->admit(req);
         }
 
@@ -150,12 +226,25 @@ map<string, string> _simulation(string trace_file, string cache_type, uint64_t c
             miss_stat.update(n_total_request, size, is_hit);
         }
 #endif
-
         ++seq;
+    }
 
-        if (!(seq%segment_window)) {
+    {//partial segment
+        auto _t_now = chrono::system_clock::now();
+        rt_seg_byte_miss.emplace_back(rt_byte_miss);
+        rt_seg_byte_req.emplace_back(rt_byte_req);
+        rt_seg_object_miss.emplace_back(rt_obj_miss);
+        rt_seg_object_req.emplace_back(rt_obj_req);
+        //real time only read rss info
+        struct proc_t usage;
+        look_up_our_self(&usage);
+        uint64_t metadata_overhead = usage.rss * getpagesize();
+        rt_seg_rss.emplace_back(metadata_overhead);
+    }
+
+    {
 #ifdef CACHE_SIZE_DECOUPLE
-            if (seq >= n_warmup) {
+        if (seq >= n_warmup) {
                 uint32_t snapshot_id = seq/segment_window;
                 cerr<<"snapshoting cache size decoupling at id: "<<snapshot_id<<endl;
                 for (auto &kv: total_request_map) {
@@ -165,23 +254,15 @@ map<string, string> _simulation(string trace_file, string cache_type, uint64_t c
                 }
             }
 #endif
-            auto _t_now = chrono::system_clock::now();
-            cerr<<"\nsegment id: " << seq/segment_window-1 << endl;
-            cerr<<"delta t: "<<chrono::duration_cast<std::chrono::milliseconds>(_t_now - t_now).count()/1000.<<endl;
-            t_now = _t_now;
-            cerr<<"segment bmr: " << double(byte_miss) / byte_req << endl;
-            seg_byte_miss.emplace_back(byte_miss); seg_byte_req.emplace_back(byte_req);
-            seg_object_miss.emplace_back(obj_miss); seg_object_req.emplace_back(obj_req);
-            byte_miss=obj_miss=byte_req=obj_req=0;
-            //reduce cache size by metadata
-            struct proc_t usage;
-            look_up_our_self(&usage);
-            uint64_t metadata_overhead = usage.rss*getpagesize();
-            seg_rss.emplace_back(metadata_overhead);
-            if (is_metadata_in_cache_size)
-                webcache->setSize(cache_size-metadata_overhead);
-            cerr<<"rss: "<<metadata_overhead<<endl;
-        }
+        seg_byte_miss.emplace_back(byte_miss);
+        seg_byte_req.emplace_back(byte_req);
+        seg_object_miss.emplace_back(obj_miss);
+        seg_object_req.emplace_back(obj_req);
+        //reduce cache size by metadata
+        struct proc_t usage;
+        look_up_our_self(&usage);
+        uint64_t metadata_overhead = usage.rss * getpagesize();
+        seg_rss.emplace_back(metadata_overhead);
     }
 
     infile.close();
@@ -192,6 +273,11 @@ map<string, string> _simulation(string trace_file, string cache_type, uint64_t c
             {"segment_object_miss", json(seg_object_miss).dump()},
             {"segment_object_req", json(seg_object_req).dump()},
             {"segment_rss", json(seg_rss).dump()},
+            {"real_time_segment_byte_miss", json(rt_seg_byte_miss).dump()},
+            {"real_time_segment_byte_req", json(rt_seg_byte_req).dump()},
+            {"real_time_segment_object_miss", json(rt_seg_object_miss).dump()},
+            {"real_time_segment_object_req", json(rt_seg_object_req).dump()},
+            {"real_time_segment_rss", json(rt_seg_rss).dump()},
 #ifdef MISS_DECOUPLE
             {"miss_decouple", miss_stat.dump()},
 #endif
--- src/simulation_future.cpp
+++ src/simulation_future.cpp
@@ -14,6 +14,7 @@
 #include "miss_decouple.h"
 #include "cache_size_decouple.h"
 #include "nlohmann/json.hpp"
+#include <proc/readproc.h>
 
 using namespace std;
 using namespace chrono;
@@ -25,7 +26,7 @@ map<string, string> _simulation_future(string trace_file, string cache_type, uin
     //annotate a file
     //not necessary to annotate, but it's easier
     if (params.find("n_extra_fields") == params.end()) {
-        cerr<<"error: field n_extra_fields is required"<<endl;
+        cerr << "error: field n_extra_fields is required" << endl;
         exit(-1);
     }
     annotate(trace_file, stoul(params["n_extra_fields"]));
@@ -42,14 +43,22 @@ map<string, string> _simulation_future(string trace_file, string cache_type, uin
 
     bool uni_size = false;
     uint64_t segment_window = 10000000;
+    uint64_t real_time_segment_window = 10000000;
     uint n_extra_fields = 0;
+    bool is_metadata_in_cache_size = true;
 
     for (auto it = params.cbegin(); it != params.cend();) {
         if (it->first == "uni_size") {
             uni_size = static_cast<bool>(stoi(it->second));
             it = params.erase(it);
+        } else if (it->first == "is_metadata_in_cache_size") {
+            is_metadata_in_cache_size = static_cast<bool>(stoi(it->second));
+            it = params.erase(it);
         } else if (it->first == "segment_window") {
             segment_window = stoull((it->second));
+            ++it;
+        } else if (it->first == "real_time_segment_window") {
+            real_time_segment_window = stoull((it->second));
             it = params.erase(it);
         } else if (it->first == "n_extra_fields") {
             n_extra_fields = stoull(it->second);
@@ -86,11 +95,27 @@ map<string, string> _simulation_future(string trace_file, string cache_type, uin
         infile.clear();
         infile.seekg(0, ios::beg);
     }
-    uint64_t tmp, id, size, next_seq;
+    uint64_t t, id, size, next_seq;
     //measure every segment
     uint64_t byte_req = 0, byte_miss = 0, obj_req = 0, obj_miss = 0;
+    //rt: real_time
+    uint64_t rt_byte_req = 0, rt_byte_miss = 0, rt_obj_req = 0, rt_obj_miss = 0;
     //global statistics
     vector<uint64_t> seg_byte_req, seg_byte_miss, seg_object_req, seg_object_miss;
+    vector<uint64_t> seg_rss;
+    //rt: real_time
+    vector<uint64_t> rt_seg_byte_req, rt_seg_byte_miss, rt_seg_object_req, rt_seg_object_miss;
+    vector<uint64_t> rt_seg_rss;
+
+    uint64_t time_window_end;
+    {
+        // Zhenyu: not assume t start from any constant, so need to compute the first window
+        infile >> next_seq >> t;
+        time_window_end =
+                real_time_segment_window * (t / real_time_segment_window + (t % real_time_segment_window != 0));
+        infile.clear();
+        infile.seekg(0, ios::beg);
+    }
 
     vector<uint16_t > extra_features(n_extra_fields, 0);
 
@@ -110,7 +135,7 @@ map<string, string> _simulation_future(string trace_file, string cache_type, uin
     CacheSizeStatistics size_stat;
 #endif
 
-    while (infile >> next_seq >> tmp >> id >> size) {
+    while (infile >> next_seq >> t >> id >> size) {
         for (int i = 0; i < n_extra_fields; ++i)
             infile>>extra_features[i];
         if (uni_size)
@@ -118,7 +143,56 @@ map<string, string> _simulation_future(string trace_file, string cache_type, uin
 
         DPRINTF("seq: %lu\n", seq);
 
+        while (t >= time_window_end) { // in seconds
+            rt_seg_byte_miss.emplace_back(rt_byte_miss);
+            rt_seg_byte_req.emplace_back(rt_byte_req);
+            rt_seg_object_miss.emplace_back(rt_obj_miss);
+            rt_seg_object_req.emplace_back(rt_obj_req);
+            rt_byte_miss = rt_obj_miss = rt_byte_req = rt_obj_req = 0;
+            //real time only read rss info
+            struct proc_t usage;
+            look_up_our_self(&usage);
+            uint64_t metadata_overhead = usage.rss * getpagesize();
+            rt_seg_rss.emplace_back(metadata_overhead);
+            time_window_end += real_time_segment_window;
+        }
+
+        if (seq && !(seq % segment_window)) {
+#ifdef CACHE_SIZE_DECOUPLE
+            if (seq >= n_warmup) {
+                uint32_t snapshot_id = seq/segment_window;
+                cerr<<"snapshoting cache size decoupling at id: "<<snapshot_id<<endl;
+                for (auto &kv: total_request_map) {
+                    if (webcache->has(kv.first)) {
+                        size_stat.update(snapshot_id, kv.second, size_map[kv.first]);
+                    }
+                }
+            }
+#endif
+            auto _t_now = chrono::system_clock::now();
+            cerr << "\nsegment id: " << seq / segment_window - 1 << endl;
+            cerr << "delta t: " << chrono::duration_cast<std::chrono::milliseconds>(_t_now - t_now).count() / 1000.
+                 << endl;
+            t_now = _t_now;
+            cerr << "segment bmr: " << double(byte_miss) / byte_req << endl;
+            seg_byte_miss.emplace_back(byte_miss);
+            seg_byte_req.emplace_back(byte_req);
+            seg_object_miss.emplace_back(obj_miss);
+            seg_object_req.emplace_back(obj_req);
+            byte_miss = obj_miss = byte_req = obj_req = 0;
+            //reduce cache size by metadata
+            struct proc_t usage;
+            look_up_our_self(&usage);
+            uint64_t metadata_overhead = usage.rss * getpagesize();
+            seg_rss.emplace_back(metadata_overhead);
+            if (is_metadata_in_cache_size)
+                webcache->setSize(cache_size - metadata_overhead);
+            cerr << "rss: " << metadata_overhead << endl;
+        }
+
+
         update_metric_req(byte_req, obj_req, size)
+        update_metric_req(rt_byte_req, rt_obj_req, size)
 
 #ifdef MISS_DECOUPLE
         //count total request
@@ -137,6 +211,7 @@ map<string, string> _simulation_future(string trace_file, string cache_type, uin
         bool is_hit = webcache->lookup(req);
         if (!is_hit) {
             update_metric_req(byte_miss, obj_miss, size)
+            update_metric_req(rt_byte_miss, rt_obj_miss, size)
             webcache->admit(req);
         }
 
@@ -146,12 +221,25 @@ map<string, string> _simulation_future(string trace_file, string cache_type, uin
             miss_stat.update(n_total_request, size, is_hit);
         }
 #endif
-
         ++seq;
+    }
 
-        if (!(seq%segment_window)) {
+    {//partial segment
+        auto _t_now = chrono::system_clock::now();
+        rt_seg_byte_miss.emplace_back(rt_byte_miss);
+        rt_seg_byte_req.emplace_back(rt_byte_req);
+        rt_seg_object_miss.emplace_back(rt_obj_miss);
+        rt_seg_object_req.emplace_back(rt_obj_req);
+        //real time only read rss info
+        struct proc_t usage;
+        look_up_our_self(&usage);
+        uint64_t metadata_overhead = usage.rss * getpagesize();
+        rt_seg_rss.emplace_back(metadata_overhead);
+    }
+
+    {
 #ifdef CACHE_SIZE_DECOUPLE
-            if (seq >= n_warmup) {
+        if (seq >= n_warmup) {
                 uint32_t snapshot_id = seq/segment_window;
                 cerr<<"snapshoting cache size decoupling at id: "<<snapshot_id<<endl;
                 for (auto &kv: total_request_map) {
@@ -161,17 +249,15 @@ map<string, string> _simulation_future(string trace_file, string cache_type, uin
                 }
             }
 #endif
-            auto _t_now = chrono::system_clock::now();
-            cerr<<"\nsegment id: " << seq/segment_window-1 << endl;
-            cerr<<"delta t: "<<chrono::duration_cast<std::chrono::milliseconds>(_t_now - t_now).count()/1000.<<endl;
-            t_now = _t_now;
-            cerr << "segment bmr: " << double(byte_miss) / byte_req << endl;
-            seg_byte_miss.emplace_back(byte_miss);
-            seg_byte_req.emplace_back(byte_req);
-            seg_object_miss.emplace_back(obj_miss);
-            seg_object_req.emplace_back(obj_req);
-            byte_miss = obj_miss = byte_req = obj_req = 0;
-        }
+        seg_byte_miss.emplace_back(byte_miss);
+        seg_byte_req.emplace_back(byte_req);
+        seg_object_miss.emplace_back(obj_miss);
+        seg_object_req.emplace_back(obj_req);
+        //reduce cache size by metadata
+        struct proc_t usage;
+        look_up_our_self(&usage);
+        uint64_t metadata_overhead = usage.rss * getpagesize();
+        seg_rss.emplace_back(metadata_overhead);
     }
 
     infile.close();
@@ -181,6 +267,12 @@ map<string, string> _simulation_future(string trace_file, string cache_type, uin
             {"segment_byte_req", json(seg_byte_req).dump()},
             {"segment_object_miss", json(seg_object_miss).dump()},
             {"segment_object_req", json(seg_object_req).dump()},
+            {"segment_rss", json(seg_rss).dump()},
+            {"real_time_segment_byte_miss", json(rt_seg_byte_miss).dump()},
+            {"real_time_segment_byte_req", json(rt_seg_byte_req).dump()},
+            {"real_time_segment_object_miss", json(rt_seg_object_miss).dump()},
+            {"real_time_segment_object_req", json(rt_seg_object_req).dump()},
+            {"real_time_segment_rss", json(rt_seg_rss).dump()},
 #ifdef MISS_DECOUPLE
             {"miss_decouple", miss_stat.dump()},
 #endif
