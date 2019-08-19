9610557f5e8b515b53e97ec93999c4f2708a01df
zhenyus <sunnyszy@gmail.com>
2019-08-19 09:38:58 -0400

    fix: awk for counting one hit wonder

--- script/trace_info.sh
+++ script/trace_info.sh
@@ -2,6 +2,6 @@
 # get byte, unique byte, unique count, average byte
  head -n 200000000 memc_200m.tr | awk '{size[$2]=$3; sum+=$3;frequency[$2]+=1}
  (!(NR%1000000)){print NR}
- END{for (i in size){unique_sum +=size[i]; count += 1; n_one_hit_wonder += 1;};
+ END{for (i in size){unique_sum +=size[i]; count += 1; if (frequency[i]==1) {n_one_hit_wonder += 1}};
   print sum, unique_sum, count, unique_sum/count, n_one_hit_wonder}'
 cat top_m_gbdt_128G_0.log |awk -F' ' '{print $5,$6,$9}'>top_m_gbdt_128G_0.log.awk
