344da2355a2e1bb84e712a3910cf69e6a32d19ca
zhenyus <sunnyszy@gmail.com>
2019-08-19 21:25:21 -0400

    feat: add scripts to build webcachesim on remote side

---
+++ script/build_remote.sh
@@ -0,0 +1,5 @@
+#!/bin/#!/usr/bin/env bash
+
+ssh -t fat 'cd /data/zhenyus/webcachesim/build; git pull; git checkout no_force_eviction; git pull; make -j32;'
+
+ssh -t clstore01.clemson.cloudlab.us 'cd /proj/cops-PG0/workspaces/zhenyus/webcachesim/build; git pull; git checkout no_force_eviction; git pull; make -j32;'
