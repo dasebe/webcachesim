cbea69c39337ead641a01a8375baf23b8e9b9230
zhenyus <sunnyszy@gmail.com>
2019-08-29 11:10:12 -0400

    update: must specific 4 config files when running pywebcachesim

--- pywebcachesim/runner/parser.py
+++ pywebcachesim/runner/parser.py
@@ -13,10 +13,10 @@ def parse_cmd_args():
                         help='debug mode only run 1 task locally',
                         type=bool,
                         choices=[True, False])
-    parser.add_argument('--config_file', type=str, nargs='?', help='runner configuration file')
-    parser.add_argument('--algorithm_param_file', type=str, help='algorithm parameter config file')
-    parser.add_argument('--trace_param_file', type=str, help='trace parameter config file')
-    parser.add_argument('--nodefile', type=str, help='node file to use for gnu parallel')
+    parser.add_argument('--config_file', type=str, nargs='?', help='runner configuration file', required=True)
+    parser.add_argument('--algorithm_param_file', type=str, help='algorithm parameter config file', required=True)
+    parser.add_argument('--trace_param_file', type=str, help='trace parameter config file', required=True)
+    parser.add_argument('--nodefile', type=str, help='node file to use for gnu parallel', required=True)
     args = parser.parse_args()
 
     return vars(args)
